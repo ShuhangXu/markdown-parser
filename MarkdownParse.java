c099f20ca30744e3b24c339554eab4219dec884c
ShuhangXu <xushuhang0805@gmail.com>
2022-04-21 23:02:41 -0700

    add two .jar files to lib

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -5,19 +5,60 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
+import javax.lang.model.util.ElementScanner14;
+
 public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+        System.out.println(currentIndex);
+        System.out.println(markdown.length());
         while(currentIndex < markdown.length()) {
+
             int openBracket = markdown.indexOf("[", currentIndex);
+            //System.out.println(openBracket);
+
             int closeBracket = markdown.indexOf("]", openBracket);
+            //System.out.println(closeBracket);
+
             int openParen = markdown.indexOf("(", closeBracket);
+            //System.out.println(openParen);
+
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            //System.out.println(closeParen);
+            
+            if(openBracket == -1 || closeBracket == -1 || openParen == -1 || closeParen == -1){
+                // In this iteration, we don't have enough []{}
+                currentIndex = markdown.length();
+                break;
+            }
+            else if(closeBracket + 1 == openParen && openBracket != -1){
+                if(markdown.indexOf("\n", openBracket-1) != openBracket-1){
+                    currentIndex = closeParen + 1;
+                }
+                else if(markdown.indexOf("\n", openBracket) < closeParen){
+                    // There is \n between openBracket and closeParen
+                    // In other words, they are not on the same line
+                    currentIndex = closeParen + 1;
+                }
+                else if(markdown.charAt(openBracket - 1) == '!'){
+                    currentIndex = closeParen + 1;
+                }
+                else{
+                    toReturn.add(markdown.substring(openParen + 1, closeParen));
+                    currentIndex = closeParen + 1;
+                }
+                
+            }
+            else{
+                break;
+            }
+            /* toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1; */
+
+            //System.out.println(currentIndex);
         }
 
         return toReturn;
